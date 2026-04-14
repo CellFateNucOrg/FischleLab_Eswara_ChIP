33bd71bea0e677ad3ea2e99c5f2ca8bec0c6a1b4
jsemple19 <8479067+jsemple19@users.noreply.github.com>
2026-04-14 12:53:27 +0200

    recovering deleted files

---
+++ makeSampleSheet.R
@@ -0,0 +1,25 @@
+library(dplyr)
+
+workDir="/Volumes/external.data/MeisterLab/FischleLab_KarthikEswara/ChIP"
+
+fqlist<-read.delim(paste0(workDir,"/fastqList.txt"),header=F)
+
+fqlist$sample<-sapply(strsplit(fqlist$V1, "/"),"[[",8)
+
+sampleSheet<-data.frame(group="",
+                        fastq_1=fqlist$V1[seq(1,nrow(fqlist),2)],
+                        fastq_2=fqlist$V1[seq(2,nrow(fqlist),2)],
+                        replicate="",antibody="",
+                        control="",control_replicate="")
+
+samples<-sapply(strsplit(sampleSheet$fastq_1, "/"),"[[",8)
+
+if(all(samples==sapply(strsplit(sampleSheet$fastq_2, "/"),"[[",8))){
+  sampleSheet$replicate<-gsub("rep","",sapply(strsplit(samples, "_"),"[[",3))
+  sampleSheet$group<-gsub("_rep[0-9]+","",samples)
+  sampleSheet$antibody[grepl("_IP",sampleSheet$group)]<-"H3K9me"
+  sampleSheet$control[sampleSheet$antibody=="H3K9me"]<-gsub("_IP","_input",sampleSheet$group)
+  sampleSheet$control_replicate[sampleSheet$antibody=="H3K9me"]<- sampleSheet$replicate[sampleSheet$antibody=="H3K9me"]
+}
+head(sampleSheet)
+write.csv(sampleSheet,paste0(workDir,"/sampleSheet.csv"), row.names=F, quote=F)
